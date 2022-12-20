93143b254c2ecbf44a61ab66d5959ea808b825fe
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2022-12-20 14:33:55 +0100

    Tweaking Bond es calc

--- ES calc/Bond 1991 example.R
+++ ES calc/Bond 1991 example.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 
 Bond_1991 <- tibble(
   trt = c("ACT", "RG", "Control"),
@@ -17,11 +17,13 @@ Bond_1991_es_T <-
     trt = c("ACT", "RG"),
     
     outcome = "Hospital days",
+    timing = "6 months",
     
     cor = 0.5,
     
     Nt = c(28, 23),
     Nc = 40,
+    N_tot = Nt + Nc,
     
     d_pp_trt = c(d_pair[1], d_pair[2]),
     d_pp_ctr = d_pair[3]
@@ -33,6 +35,14 @@ Bond_1991_es_T <-
     d = d_pp_trt - d_pp_ctr,
     vd = (1/Nt + 1/Nc) * 2*(1-cor) + d^2/(2*(Nt+Nc-2)),
     
-  
+    # Gleser & Olkin 2009 eq 19.19
+    cov_ds = 1/Nc + (prod(d)/ 2*(N_tot - 2 - 1)),
     
+    J = 1 - 3/(4*N_tot-1),
+    
+    g = J * d,
+    vg = J^2 * vd
+   
+    #Make cluster bias correction below
+     
   ); Bond_1991_es_T
