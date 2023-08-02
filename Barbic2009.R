f6229125178f4a4e8a61c4e97864c703f73c2c47
jasavive <119591317+jasavive@users.noreply.github.com>
2023-08-02 09:48:47 +0200

    Add files via upload

--- Barbic2009.R
+++ Barbic2009.R
@@ -14,6 +14,21 @@ library(ggplot2)
 library(tidyverse)
 
 
+# Setting the relevant options -------------------
+# The code benith sets global options in R for various packages and functions
+options(pillar.sigfig = 4) # ensure tibble include 4 digits
+
+options(tibble.width = Inf) # This sets the width of output from the tibble 
+# package to infinity, meaning that the output will 
+# not be truncated or wrapped.
+
+options(dplyr.print_min = 310) # This sets the minimum number of rows that will 
+# be printed when using the dplyr package to 310
+
+options(scipen = 10) # This sets the scientific notation precision in R to 10, 
+# meaning that numbers will be displayed with 10 digits of precision
+
+
 Barbic2009 <- tibble(
   Outcome = rep(c("Hope Index", "Empowerment scale", "Quality of life", "Recovery assessment"), each = 2),
   Treatment = rep(c("Recovery workbook", "Control group"), dplyr::n_distinct(Outcome)),
@@ -44,4 +59,5 @@ Barbic2009 <- tibble(
     20.11, 22.09)
 )
 
+Barbic2009
 View(Barbic2009)
