bd1bb4673ea2811c94fa3a7b7b2669352be732b5
Jakob Kaarup Jensen <B280036@PROD.SITAD.DK>
2023-12-18 15:51:58 +0100

    commit message

--- Sacks2011.R
+++ Sacks2011.R
@@ -23,7 +23,7 @@ options(scipen = 10) # This sets the scientific notation precision in R to 10,
 # in the same tibble - "sample" indicates different propencity category. 
 #Extracted from table 4 p. 1682 and only means plus standard deviation is taken.
 
-sack_2011 <- tibble(
+sacks_2011 <- tibble(
   
   group = rep(c("treatment",
                 "control"), 
@@ -60,77 +60,82 @@ sack_2011 <- tibble(
             44.9, 47.2, # SF-36 physical health
             
             # High propensity
-            6.3, 7.5,
-            15.9, 8.3,
-            15.3, 15.3,
-            42.0, 41.8,
-            43.5, 43.1,
-            13.2, 43.6,
-            3.0, 3.5,
-            42.0, 42.6
+            6.3, 7.5, # alcohol
+            15.9, 8.3, # drugs
+            15.3, 15.3, # BDI
+            42.0, 41.8, # GSI
+            43.5, 43.1, # SF-36 mental
+            13.2, 43.6, # SF-36 social functioning
+            3.0, 3.5, # Health rating
+            42.0, 42.6 # SF-36 physical health
             
             ),
   
   
-  sd_pre = c(2.4, 2.2,
-             7.2, 8.7,
-             6.7, 10.0,
-             4.0, 9.0,
-             11.8, 13.0,
-             11.3, 13.7,
-             1.2, 1.1,
-             9.0, 10.3,
+  sd_pre = c(# Low/medium propensity 
+             2.4, 2.2, # alcohol
+             7.2, 8.7, # drugs
+             6.7, 10.0, # BDI
+             4.0, 9.0, # GSI# SF-36 mental
+             11.8, 13.0, # SF-36 mental
+             11.3, 13.7, # SF-36 social functioning
+             1.2, 1.1, # Health rating
+             9.0, 10.3, # SF-36 physical health
              
-             2.3, 0.7,
-             7.5, 5.7,
-             3.7, 11.9,
-             10.3, 7.4,
-             10.4, 9.1,
-             13.9, 11.3,
-             1.0, 0.6,
-             10.4, 14.0),
+             # High propensity
+             2.3, 0.7, # alcohol
+             7.5, 5.7, # drugs
+             3.7, 11.9, # BDI
+             10.3, 7.4, # GSI
+             10.4, 9.1, # SF-36 mental
+             13.9, 11.3, # SF-36 social functioning
+             1.0, 0.6, # Health rating
+             10.4, 14.0), # SF-36 physical health
   
   
   m_post = c(
-    2.7, 0.6,
-    0.6, 2.1,
-    13.1, 8.2,
-    38.6, 36.4,
-    46.5, 47.2,
-    44.9, 46.5,
-    2.9, 3.0,
-    45.5, 47.6,
+    # Low/medium propensity 
+    2.7, 0.6, # alcohol
+    0.6, 2.1, # drugs
+    13.1, 8.2, # BDI
+    38.6, 36.4, # GSI
+    46.5, 47.2, # SF-36 mental
+    44.9, 46.5, # SF-36 social functioning
+    2.9, 3.0, # Health rating
+    45.5, 47.6, # SF-36 physical health
     
-    
-    0.4, 1.0,
-    0.4, 1.3,
-    10.0, 16.5, 
-    39.0, 42.3,
-    49.5, 41.6, 
-    47.5, 46.3,
-    3.1, 4.3,
-    42.8, 48.8
+    # High propensity
+    0.4, 1.0, # alcohol
+    0.4, 1.3, # drugs
+    10.0, 16.5, # BDI
+    39.0, 42.3, # GSI
+    49.5, 41.6,  # SF-36 mental
+    47.5, 46.3, # SF-36 social functioning
+    3.1, 4.3, # Health rating
+    42.8, 48.8 # SF-36 physical health
   ),
   
   sd_post = c(
-    2.1, 1.8,
-    1.9, 4.7,
-    11.9, 6.6,
-    9.2, 10.1, 
-    14.2, 13.7,
-    12.6, 11.5,
-    1.2, 1.0,
-    7.3, 10.1, 
+    # Low/medium propensity 
+    2.1, 1.8, # alcohol
+    1.9, 4.7, # drugs
+    11.9, 6.6, # BDI
+    9.2, 10.1, # GSI
+    14.2, 13.7, # SF-36 mental
+    12.6, 11.5, # SF-36 social functioning
+    1.2, 1.0, # Health rating
+    7.3, 10.1,  # SF-36 physical health
     
-    1.1, 1.4,
-    0.9, 1.9,
-    10.9, 8.4,
-    7.7, 5.9,
-    11.1, 13.2, 
-    12.0, 7.7,
-    1.0, 0.5,
-    12.9, 6.6
+    # High propensity
+    1.1, 1.4, # alcohol# drugs
+    0.9, 1.9, # drugs
+    10.9, 8.4, # BDI
+    7.7, 5.9, # GSI
+    11.1, 13.2,  # SF-36 mental
+    12.0, 7.7, # SF-36 social functioning
+    1.0, 0.5, # Health rating
+    12.9, 6.6 # SF-36 physical health
   )
   
   
-); sack_2011
+); sacks_2011
