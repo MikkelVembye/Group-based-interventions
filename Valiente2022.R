c666da034b8ea07372e3fdbb0c81325db045677c
Jakob Kaarup Jensen <B280036@PROD.SITAD.DK>
2024-01-05 13:13:23 +0100

    Adding Valiente2022

--- Valiente2022.R
+++ Valiente2022.R
@@ -77,7 +77,30 @@ valiente2022  <- tibble(
     1.14, 0.99
     
   ),
-
+  
+  sd_pre = c(
+    # Table 3
+    4.4,  4.2,
+    8.0, 7.5,
+    8.7, 7.8,
+    7.8, 7.3,
+    7.7, 5.9,
+    7.4, 6.8,
+    7.1, 6.8,
+    
+    # Table 4
+    0.83, 0.81,
+    0.87, 0.93,
+    0.81, 0.95,
+    0.87, 0.89,
+    0.82, 0.97,
+    0.83, 0.80,
+    0.77, 1.01,
+    0.86, 0.86,
+    0.82, 0.83
+    
+  ),
+        
  
   
   m_post = c(
@@ -90,7 +113,6 @@ valiente2022  <- tibble(
     35.5, 35.5,
     19.0, 19.2,
     
-    
     # Table 4
     0.90, 0.96,
     1.50, 1.48,
@@ -101,7 +123,80 @@ valiente2022  <- tibble(
     0.91, 1.07,
     1.18, 1.21,
     0.99, 0.98
+  ),
+  
+  sd_post = c(
+    # Table 3
+    3.4, 4.2, 
+    7.1, 7.6, 
+    7.5, 7.8, 
+    7.5, 7.3, 
+    6.9, 5.9,
+    6.8, 6.8,
+    6.6, 6.8,
+    
+    # Table 4
+    0.82, 0.84,
+    0.86, 0.96,
+    0.79, 0.89,
+    0.80, 0.94,
+    0.84, 0.91,
+    0.76, 0.79,
+    0.80, 0.97,
+    0.90, 0.87,
+    0.85, 0.86
+  )
+    
+)
+
+# Making the tibble wide 
+
+valiente2022_wide <-
+  valiente2022 |> 
+  mutate (group = case_match(
+    group, "Multicomponent PPI"  ~ "t", "Treatment as usual" ~ "c")) |> 
+  tidyr::pivot_wider(
+    names_from = group,
+    names_glue = "{.value}_{group}",
+    values_from = N:last_col()
   )
   
-)
-  
+# Effect sizes: Estimating G and D (pre-post test) (Not finished yest)
+valiente2022_est <-           
+  valiente2022_wide |>
+  mutate(
+    analysis_plan = rep(c(
+                      "Scales of Psychological Well-Being (SPWB)",
+                      "Satisfaction With Life Scale (SWLS)",
+                      "Symptom checklist-90-Revised (SCL-90-R)"
+                      ), c(6,1,9))
+    
+    
+  )|> 
+  rowwise() |> 
+  mutate(
+    study = "Valiente et al. 2022",
+    
+    N_total = N_t + N_c,
+    df_ind = N_total,
+    
+    # The outcome SCL-90-R reverted because lower score is beneficial
+    m_post = if_else(analysis_plan != "Symptom checklist-90-Revised (SCL-90-R)", 
+                     m_post_t - m_post_c,
+                     (m_post_t - m_post_c) * -1),
+    
+    sd_pool = sqrt(((N_t-1)*sd_post_t^2 + (N_c-1)*sd_post_c^2)/(N_t + N_c - 2)),  
+    
+    d_post = m_post/sd_pool, 
+    vd_post = (1/N_t + 1/N_c) + d_post^2/(2*df_ind),
+    Wd_post = (1/N_t + 1/N_c),
+    
+    J = 1 - 3/(4*df_ind-1),
+    
+    g_post = J * d_post,
+    vg_post = (1/N_t + 1/N_c) + g_post^2/(2*df_ind),
+    Wg_post = Wd_post,
+    
+  ) |> 
+  ungroup()
+
