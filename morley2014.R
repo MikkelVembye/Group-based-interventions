e544f79506159225da9b6ccfdf84e750c6623ced
Jakob Kaarup Jensen <B280036@PROD.SITAD.DK>
2024-03-01 15:13:06 +0100

    Updating Morley et al. 2014

--- morley2014.R
+++ morley2014.R
@@ -19,9 +19,9 @@ options(dplyr.print_min = 310) # This sets the minimum number of rows that will
 options(scipen = 10) # This sets the scientific notation precision in R to 10, 
 # meaning that numbers will be displayed with 10 digits of precision
 
-# Data from table 2 containing means and standard deviation on page 351
+# Data from table 2 containing means and standard deviation on page 136
 
-morley2014 <- tibble(
+Morley2014 <- tibble(
   group = as.factor(rep(c("Intervention", 
                           "Control"), 
                         each = 1,4)),
@@ -32,7 +32,7 @@ morley2014 <- tibble(
                             "SAS"), each = 2,1
   )),
   
-  n = rep(c(
+  N = rep(c(
     122, 63
   ), each = 1,4),
   
@@ -64,6 +64,61 @@ morley2014 <- tibble(
     12.49, 9.58
   ),
   
-  m_diff = m_post - m_pre
+); Morley2014
+
+
+
+# Turning dataset into wide format
+Morley2014_wide <-
+  Morley2014 |> 
+  mutate (group = case_match(
+    group, "Intervention"  ~ "t", 
+    "Control" ~ "c")) |> 
+  tidyr::pivot_wider(
+    names_from = group,
+    names_glue = "{.value}_{group}",
+    values_from = N:last_col()
+  )
+
+
+# Effect size calculating 
+Morley2014_est <-           
+  Morley2014_wide |>
+  mutate(
+    analysis_plan =
+      c("Suicide Ideation",
+        "Hospital Anxiety and Depression Scale",
+        "Hospital Anxiety and Depression Scale",
+        "Self-Efficacy Scale"
+      )
+    
+  ) |> 
   
-  ); morley2014
+  rowwise() |> 
+  mutate(
+    study = "Morley et al. 2014",
+    
+    N_total = N_t + N_c,
+    df_ind = N_total,
+    
+    # Reverting m_post for specific outcomes
+    m_post = if_else(outcome %in% c("BSS", "HADS_depression", "HADs_anxiety"),
+                     (m_post_t - m_post_c) * -1, 
+                     m_post_t - m_post_c),
+    
+    sd_pool = sqrt(((N_t-1)*sd_post_t^2 + (N_c-1)*sd_post_c^2)/(N_t + N_c - 2)),  
+    
+    d_post = m_post/sd_pool, 
+    vd_post = (1/N_t + 1/N_c) + d_post^2/(2*df_ind),
+    Wd_post = (1/N_t + 1/N_c),
+    
+    J = 1 - 3/(4*df_ind-1),
+    
+    g_post = J * d_post,
+    vg_post = (1/N_t + 1/N_c) + g_post^2/(2*df_ind),
+    Wg_post = Wd_post,
+    
+    vary_id = outcome
+    
+  ) |> 
+  ungroup()
