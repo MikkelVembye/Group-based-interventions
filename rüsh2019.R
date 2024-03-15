ddbf544f2d010d635dd9f988d9dd1984b463b7eb
Jakob Kaarup Jensen <B280036@PROD.SITAD.DK>
2024-03-15 11:59:22 +0100

    update

--- rüsh2019.R
+++ rüsh2019.R
@@ -22,35 +22,48 @@ options(scipen = 10) # This sets the scientific notation precision in R to 10,
 # Data from table 1 containing means and standard deviation on page 335
 # The table also contains partial η2 values, p-values and F-test
 
-rusch2019 <- tibble(
+Rüsch2019 <- tibble(
   group = as.factor(rep(c("Intervention", 
                           "Control"), 
-                        each = 1,7)),
+                        each = 1,14)),
   
-  outcome = as.factor(rep(c("Job_search",
-                            "Help_seeking",
+  timing = rep(c("6w", 
+                 "12w"), 
+               each = 14,1),
+
+  outcome = as.factor(rep(c("Job_search", 
+                            "Help_seeking", 
                             "SISR",
                             "SSMIS_SF",
-                            "CES-D",
-                            "BHS",
-                            "Secrecy"), each = 2,1
-  )),
+                            "CES-D", 
+                            "BHS",  
+                            "Secrecy"), 
+                          each = 2,2 )
+                      ),
   
-  n_pre = rep(c(
+  N = c(rep(c(
+    18, 17), # 6 weeks 
+     times = 7), 
+        rep(c(
+    
+    20, 13), # 12 weeks
+    times = 7)),
+  
+  N_start = rep(c(
     23, 19
-  ), each = 1,7),
+  ), each = 1,14),
   
-  m_pre = c(
+  m_pre = rep(c(
     3.1, 3.3, # Job-search self-efficacy (1–5)
     3.4, 3.0,  # Help-seeking intentions (1–7)
     15.3, 16.3, # Recovery (SISR: 4–24)
     17.3, 17.3,  # Self-stigma (SSMIS-SF: 5–45)
     39.2, 41.4, # Depressive symptoms (CES-D: 15–60)
     14.9, 14.1, # Hopelessness (BHS: 4–24)
-    4.0, 3.7   # Hopelessness (BHS: 4–24)
-  ),
+    4.0, 3.7   # Link’s 5-item Secrecy Scale
+  ), each = 1,2),
   
-  sd_pre = c(
+  sd_pre = rep(c(
     0.8, 0.8,
     1, 1,
     4.1, 4.2,
@@ -58,61 +71,28 @@ rusch2019 <- tibble(
     7.4, 7.2,
     3.6, 4.5, 
     1.2, 1.6
-  ),
+  ), each = 1,2),
   
-  n_3weeks = rep(c(
-    21, 17
-  ), each = 1,7),
-  
-  m_3weeks = c(
-    3.2, 3.2, 
-    3.6, 3,
-    16.1, 15.9, 
-    15.3, 16.9, 
-    35.2, 39.1,
-    13.8, 13.7,
-    3.8, 3.7
-  ),
-  
-  sd_3weeks = c(
-    0.7, 0.9, 
-    1, 0.9, 
-    4.2, 3.6, 
-    6.2, 8, 
-    8.8, 10.2, 
-    3.9, 4.6, 
-    1, 1.7
-  ),
-  
-  n_6weeks = rep(c(
-    18, 17
-  ), each = 1,7),
-  
-  m_6weeks = c(
+  m_post = c(
+    # 3 Weeks
+  #  3.2, 3.2, 
+  #  3.6, 3,
+  #  16.1, 15.9, 
+  #  15.3, 16.9, 
+  #  35.2, 39.1,
+  #  13.8, 13.7,
+  #  3.8, 3.7
+    
+  # 6 weeks
     3.4, 3.3, 
     3.5, 3.2, 
     17.8, 16.1, 
     14.6, 19.1, 
     31.5, 40.1, 
     12.6, 14.2, 
-    3.5, 3.9
-  ),
-  
-  sd_6weeks = c(
-    0.8, 0.9,
-    1, 1, 
-    3.4, 4.4,
-    7.9, 8.7,
-    8.5, 10.1,
-    3.6, 5, 
-    1.3, 1.7
-  ),
-  
-  n_12weeks = rep(c(
-    20, 13
-  ), each = 1,7),
-  
-  m_12weeks = c(
+    3.5, 3.9,
+    
+    # 12 weeks 
     3.2, 3.2,
     3.3, 3.1,
     17.3, 15.7,
@@ -120,23 +100,72 @@ rusch2019 <- tibble(
     32, 37.5,
     12.3, 14.4,
     3.7, 4.4
+    
   ),
   
-  sd_12weeks = c(
-    1, 0.9, 
-    0.6, 0.9,
-    3.5, 2.6,
-    7, 9.6,
-    7.6, 9.8,
-    4.2, 3,
-    0.9, 1.4
-  ),
+ 
+sd_post = c(
+  # 3 weeks
+  # 0.7, 0.9, 
+  # 1, 0.9, 
+  # 4.2, 3.6, 
+  # 6.2, 8, 
+  # 8.8, 10.2, 
+  # 3.9, 4.6, 
+  # 1, 1.7
   
-  m_diff3 = m_3weeks - m_pre,
+  # 6 weeks
+  0.8, 0.9,
+  1, 1, 
+  3.4, 4.4,
+  7.9, 8.7,
+  8.5, 10.1,
+  3.6, 5, 
+  1.3, 1.7,
   
-  m_diff6 = m_6weeks - m_pre,
-  
-  m_diff12 = m_12weeks - m_pre
+  # 12 weeks
+  1, 0.9, 
+  0.6, 0.9,
+  3.5, 2.6,
+  7, 9.6,
+  7.6, 9.8,
+  4.2, 3,
+  0.9, 1.4
+ )
+); Rüsch2019
 
-); rusch2019
 
+
+# Turning data into wide format
+params <- tibble(
+  filter_val1 = rep(c(paste0(c(6, 12), "w")), 1)
+)
+
+wide_Rüsch2019_func <- 
+  function(filter_val1){
+    
+    Rüsch2019 |> 
+      filter(timing == filter_val1) |> 
+      mutate(group = case_match(group, "Intervention" ~ "t", "Control" ~ "c")) |>
+      tidyr::pivot_wider(
+        names_from = group,
+        names_glue = "{.value}_{group}",
+        values_from = N:last_col()
+      )   
+    
+  }
+
+
+Rüsch2019_est <- 
+  pmap(params, wide_Rüsch2019_func) |>
+  list_rbind() |>
+  # Based on the degrees of freedom value reported in Druss et al. 2018 table 2 and 3
+  mutate(
+    analysis_plan = rep(
+      c(
+        "Positive And Negative Syndromes Scale (PANSS)", # PANSS
+        "Positive And Negative Syndromes Scale (PANSS)", # PANSS
+        "Positive And Negative Syndromes Scale (PANSS)", # PANSS
+        "Hamilton Depression Rating Scale (HAM-D)", # HAM-D
+        "Health-related quality of life, with the Quality of Well-Being (QWB) Scale" # QWB
+      )
